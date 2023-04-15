86bc1ceba332c0710324b2c1a3d7fa18b0a1c8e3
Justin Gallagher <justingallagher512@gmail.com>
2023-04-15 10:57:13 -0400

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+// app-routing.module.ts: This file is used to define the routing configuration for the application. 
+// It imports the necessary components and defines the routes for navigating between them.
+
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'justangular';
+}
